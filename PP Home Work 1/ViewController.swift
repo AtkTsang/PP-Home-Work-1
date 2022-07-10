301f2988b9fa6e72896e4712cb51260f60cff0a6
atktsang <atktsang@gmail.com>
2022-07-10 17:41:14 +0800

    PP Homework 1

--- PP Home Work 1/ViewController.swift
+++ PP Home Work 1/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testButton: UIButton!
+    
+    @IBOutlet weak var uselessLabel: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        uselessLabel.text = "What is my purpose?"
+        
+    
     }
-
-
+    
+    
+    @IBAction func changeText(_ sender: Any) {
+        uselessLabel.text = "Your pass butter."
+    }
+    
+    
 }
 
